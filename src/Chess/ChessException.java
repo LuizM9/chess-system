ea7f864fba9bd266a3ec6f58c93e5d9531f3a83b
Luiz Carlos <luizcmarinho9@gmail.com>
2023-06-01 14:14:35 -0300

    Handling captured pieces

--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -1,10 +1,11 @@
-package Chess;
+package chess;
 
 import boardgame.BoardException;
 
-public class ChessException extends BoardException{
+public class ChessException extends BoardException {
+	private static final long serialVersionUID = 1L;
 
-    public ChessException(String msg){
-        super(msg);
-    }
+	public ChessException(String msg) {
+		super(msg);
+	}
 }
