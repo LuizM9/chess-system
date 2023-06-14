83bca71f3bb2cd5e989eef55ff3f8f5e28588a8f
Luiz Carlos <luizcmarinho9@gmail.com>
2023-06-14 14:38:21 -0300

    Fix - better treatment to promotion and fix check

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,6 @@
 package chess;
 
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -128,7 +128,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
@@ -277,7 +277,7 @@ public class ChessMatch {
 				return (ChessPiece)p;
 			}
 		}
-		throw new IllegalStateException("There is no " + color + "king on the board");
+		throw new IllegalStateException("There is no " + color + " king on the board");
 	}
 
 	private  boolean testCheck(Color color){
@@ -296,7 +296,7 @@ public class ChessMatch {
 		if (!testCheck(color)){
 			return false;
 		}
-		List<Piece> list = piecesOnTheBoard.stream().filter(x ->((chess.ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+		List<Piece> list = piecesOnTheBoard.stream().filter(x ->((chess.ChessPiece)x).getColor() == color).collect(Collectors.toList());
 		for (Piece p : list){
 			boolean[][] mat = p.possibleMoves();
 			for (int i=0; i< board.getRows(); i++){
